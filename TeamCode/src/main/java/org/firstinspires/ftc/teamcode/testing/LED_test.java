e659d92243d5708f1b3667b11c871217258a1091
Hayk Tshorokhyan <hayktshorokhyan@Hayks-MacBook.attlocal.net>
2018-11-13 01:05:48 -0600

    TensorFlow Object Detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/LED_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/LED_test.java
@@ -42,20 +42,6 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.LED;
 import org.firstinspires.ftc.teamcode.LedColorLibrary;
 
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="LED_Test", group="Iterative Opmode")
 //@Disabled
 public class LED_test extends LinearOpMode {
@@ -63,21 +49,6 @@ public class LED_test extends LinearOpMode {
     public Servo led1 = null;
     LedColorLibrary colors = new LedColorLibrary();
     private ElapsedTime runtime = new ElapsedTime();
-
-
-//    @Override
-//    public void init() {
-//
-////        //hwMap = ahwMap;
-////
-////        // Initialize the hardware variables.
-////        led1 = hwMap.get(Servo.class, "led1");
-////
-////
-////        //Setting LED lights with power OFF when initializing
-////        led1.setPosition(colors.Twinkles_Purple_and_Black);
-//
-//    }
 
     @Override
     public void runOpMode() {
